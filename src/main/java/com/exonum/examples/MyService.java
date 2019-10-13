19b168393de6d1d4a02cdbe5cbf99782a4e4aa19
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-10-13 12:47:09 +0300

    Initial commit

---
+++ src/main/java/com/exonum/examples/MyService.java
@@ -0,0 +1,54 @@
+/* 
+ * Copyright 2018 The Exonum Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.exonum.examples;
+
+import com.exonum.binding.core.service.AbstractService;
+import com.exonum.binding.core.service.Node;
+import com.exonum.binding.core.service.Schema;
+import com.exonum.binding.core.service.TransactionConverter;
+import com.exonum.binding.core.storage.database.Fork;
+import com.exonum.binding.core.storage.database.View;
+import com.google.inject.Inject;
+import io.vertx.ext.web.Router;
+
+import java.util.Optional;
+
+public final class MyService extends AbstractService {
+
+  public static final short ID = 42;
+  static final String NAME = "my-service";
+  static final String INITIAL_SERVICE_CONFIGURATION = "{ \"version\": 0.1 }";
+
+  @Inject
+  public MyService(TransactionConverter transactionConverter) {
+    super(ID, NAME, transactionConverter);
+  }
+
+  @Override
+  protected Schema createDataSchema(View view) {
+    return new MySchema(view);
+  }
+
+  @Override
+  public Optional<String> initialize(Fork fork) {
+    return Optional.of(INITIAL_SERVICE_CONFIGURATION);
+  }
+
+  @Override
+  public void createPublicApiHandlers(Node node, Router router) {
+  }
+}
